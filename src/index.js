31cdb85e7489d9fb42fa39c8f91301e5b6a1a75a
darrionn33 <darrionn33@gmail.com>
2023-11-05 11:47:37 +0530

    added first version of adding list items

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import "./styles.css";
 import rootRender from "./rootRender";
-
-const helloWorld = () => {
-  const helloWorld = document.createElement("p");
-  helloWorld.textContent = "Hello World!";
-  return helloWorld;
+import todoList from "./components/todoList";
+import signal from "signal-js";
+const list = {
+  title: "List #1",
+  list: [
+    { title: "Poop", priority: "urgent" },
+    { title: "Eat Breakfast", priority: "moderate" },
+    { title: "Wash Dishes", priority: "chill" },
+    { title: "Bathe Ezio", priority: "chill" },
+    { title: "Beat Ezio Up", priority: "urgent" },
+  ],
 };
-
-rootRender(helloWorld(), "apc");
+signal.on("addListItem", (newListItem) => list.list.push(newListItem));
+signal.on("refreshTodoList", () => {
+  rootRender("", "rpc");
+  rootRender(todoList(list), "apc");
+});
+rootRender(todoList(list), "apc");
