4ff69593f2e25b768a05bb86b7dcf187e16178fe
darrionn33 <darrionn33@gmail.com>
2023-11-05 11:44:45 +0530

    added new list item components

---
+++ src/components/newListItemDiv.js
@@ -0,0 +1,42 @@
+import put from "put-selector";
+import signal from "signal-js";
+import svg from "svg";
+
+export default function newListItemDiv() {
+  const handleSubmit = () => {
+    if (newListItemInput.value !== "") {
+      signal.emit("addListItem", {
+        title: newListItemInput.value,
+        priority: select.value,
+      });
+    }
+    signal.emit("refreshTodoList");
+  };
+
+  const newListItemDiv = put("div.new-list-item");
+  const select = put("select");
+  put(select, "option", "chill");
+  put(select, "option", "urgent");
+  put(select, "option", "moderate");
+
+  const newListItemInput = put("input.new-list-item-input", {
+    placeholder: "Enter a task here...",
+  });
+  newListItemInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  });
+
+  const svgText =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Add Task</title><path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg>';
+
+  const checkBoxSvg = svg(svgText);
+  checkBoxSvg.addEventListener("click", handleSubmit);
+
+  put(newListItemDiv, newListItemInput);
+  put(newListItemDiv, select);
+  put(newListItemDiv, checkBoxSvg);
+
+  return newListItemDiv;
+}
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,10 +1,20 @@
 import put from "put-selector";
 import todoListItem from "./todoListItem";
+import newListItemDiv from "./newListItemDiv";
 export default function todoList(listItem) {
   const todoList = put("div.todo-list");
   put(todoList, "h2", listItem.title);
   listItem.list.forEach((listObject) => {
     put(todoList, todoListItem(listObject.title, listObject.priority));
   });
+  const newListInputButton = put("button.new-list-item-button", "+ New Task");
+  put(todoList, newListInputButton);
+
+  newListInputButton.onclick = () => {
+    put(newListInputButton, "!");
+    put(todoList, newListItemDiv());
+    document.querySelector("input.new-list-item-input").focus();
+  };
+
   return todoList;
 }
