36011bd599e82466893debd85d7f2b01af5d599f
darrionn33 <darrionn33@gmail.com>
2023-11-05 11:57:35 +0530

    implemented first version of localStorage

--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@ import "./styles.css";
 import rootRender from "./rootRender";
 import todoList from "./components/todoList";
 import signal from "signal-js";
-const list = {
-  title: "List #1",
-  list: [
-    { title: "Poop", priority: "urgent" },
-    { title: "Eat Breakfast", priority: "moderate" },
-    { title: "Wash Dishes", priority: "chill" },
-    { title: "Bathe Ezio", priority: "chill" },
-    { title: "Beat Ezio Up", priority: "urgent" },
-  ],
-};
-signal.on("addListItem", (newListItem) => list.list.push(newListItem));
+
+if (!localStorage.getItem("list")) {
+  localStorage.setItem(
+    "list",
+    JSON.stringify({
+      title: "List #1",
+      list: [
+        { title: "Poop", priority: "urgent" },
+        { title: "Eat Breakfast", priority: "moderate" },
+        { title: "Wash Dishes", priority: "chill" },
+        { title: "Bathe Ezio", priority: "chill" },
+        { title: "Beat Ezio Up", priority: "urgent" },
+      ],
+    })
+  );
+}
+const list = JSON.parse(localStorage.getItem("list"));
+signal.on("addListItem", (newListItem) => {
+  list.list.push(newListItem);
+  localStorage.setItem("list", JSON.stringify(list));
+});
 signal.on("refreshTodoList", () => {
   rootRender("", "rpc");
   rootRender(todoList(list), "apc");
