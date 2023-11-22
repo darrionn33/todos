5d5812355bbf285bbee786d6887a9fd96e7d6559
darrionn33 <darrionn33@gmail.com>
2023-11-22 17:39:47 +0530

    Revert "added service workers"
    This reverts commit ddf493e185ddc4bea3c6e9582c435d220a7b37ca.

--- src/index.js
+++ src/index.js
@@ -167,16 +167,3 @@ rootRender(settings());
 rootRender(header());
 rootRender(sidebar(lists, currentListIndex));
 rootRender(main(currentListSelector(0), currentListIndex));
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("SW registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,12 +11,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
     }),
   ],
   module: {
